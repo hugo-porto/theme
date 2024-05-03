647dd0ea41fc4e3e87b44d7eeedb571dfb093faa
Andrew Molyuk <andrew@molyuk.com>
2024-05-03 19:14:11 +0300

    Add hero section

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ['./layouts/**/*.html'],
-  theme: {
-    extend: {},
-    fontFamily: {
-      sans: ['Roboto'],
-      serif: ['Montserrat'],
-    },
-  },
   plugins: [],
   darkMode: 'class',
 };
